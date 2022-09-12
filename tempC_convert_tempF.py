0929bdba5cb34dfda3eedf92aaa8727e81b90953
8/9/2022 <l.mcuong1705@gmail.com>
2022-09-12 12:18:46 +0700

    flask

--- tempC_convert_tempF.py
+++ tempC_convert_tempF.py
@@ -1,29 +1,34 @@
 from flask import Flask
 
+
 app= Flask(__name__)
 
 @app.route('/cToFConvertter')
 def cToFConvertter():
+    
     while True:
-        
+                
         cTemp = input("Please enter C degree: ")
         if cTemp:
             fTemp = round(9* float(cTemp)/5 + 32, 1)
 
-            print(f'{cTemp}C is converted to {fTemp}F')
+            
+            return (f'{cTemp}C is converted to {fTemp}F')
+        
             break
         else:
             print('cTemp is empty')
             continue
-        
+  
 
 
 
 
 def main():
     cToFConvertter ()
+    app.run() 
 
 
 if __name__ == "__main__":
     main()
-    app.run() 
+    
