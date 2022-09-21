2a106b0670a496d0b0661c926e718501bffd649d
8/9/2022 <l.mcuong1705@gmail.com>
2022-09-21 21:56:43 +0700

    1

--- api.py
+++ api.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask import  render_template, redirect, url_for
 
 
-app= Flask(__name__)
+app=Flask(__name__,template_folder='templates')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/index')
+def index():
+    return render_template ('index.html.html')
 
 if __name__ == '__main__':
     
