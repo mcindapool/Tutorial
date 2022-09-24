7bb19caf51922d6778e4107a5202f7ceae5c1984
8/9/2022 <l.mcuong1705@gmail.com>
2022-09-24 14:54:43 +0700

    1

--- api.py
+++ api.py
@@ -4,10 +4,14 @@ from flask import  render_template, redirect, url_for
 
 app=Flask(__name__,template_folder='templates')
 
-@app.route('/index')
-def index():
-    return render_template ('index.html.html')
+@app.route('/')
+def home ():
+    return "welcome"
+
+@app.route('/login')
+def login():
+    return render_template ('login.html.html')
 
 if __name__ == '__main__':
     
-    app.run()
+ app.run(debug= True, port=8000)
