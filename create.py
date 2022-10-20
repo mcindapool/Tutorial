fb7e71af2982ae26db389ddbf1736526a7096e27
8/9/2022 <l.mcuong1705@gmail.com>
2022-10-20 12:42:55 +0700

    done

--- create.py
+++ create.py
@@ -1,10 +1,18 @@
-import time
-from selenium import webdriver
 
-driver= webdriver.Edge(executable_path="../exercise/msedgedriver.exe")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+PATH= "../exercise/chromedriver.exe"
+driver= webdriver.Chrome(PATH)
 driver.set_window_size(700,800)
 
-driver.get("http://adh.bizz.vn")
+driver.get("https://www.google.com/")
+search = driver.find_element(By.NAME,"q")
+search.send_keys('cuong')
+search.send_keys(Keys.RETURN)
 
-time.sleep(10)
-driver.quit
+
+time.sleep(60)
+driver.quit()
