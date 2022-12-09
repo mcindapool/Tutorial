dc5bd340e2b7d4ff91387968fb6060ba0fcb941d
8/9/2022 <l.mcuong1705@gmail.com>
2022-12-09 14:14:10 +0700

    09/12/2022

---
+++ js/main.js
@@ -0,0 +1,82 @@
+//  /*
+//  Var : đặt tên biến
+//  alert: tạo khung
+//  // : comment 1 dòng
+//  Crtl + /: phím tắt comment
+//  Giới thiệu một số hàm BUILT-IN FUNC
+//     1. Alert
+//     2. Console
+//     3. Confirm : input giá trị 
+//     4. Promt
+//     5. Set timeout: code được chạy sau 1 khoảng thời gian nhất định đơn bị mile giây
+//     6. Set interval: được thực thi liên tục "vòng lặp"
+//  */
+// // var fullName = "Le Manh Cuong"
+// //   var age = 24
+
+// //   alert(fullName)
+// //   alert(age)
+// // console.log(fullName)
+// // confirm('Xac nhan du tuoi!')
+// // prompt('Xac dinh du tuoi!')
+// // setTimeout(function() {
+// //     alert('Thong bao')
+// // }, 1000)
+// // setInterval(function() {
+// //     console.log('thong bao')
+// // }, 1000)
+
+// /**
+//  * toán tử số học - Arithmetic
+//  * toán tử gán - Assignment
+//  * toán tử so sánh - Comparison
+//  * toán tử Logic - Logical
+//       - 1. && - And
+//       - 2. || - Or
+//       - 3. ! - Not
+//  */
+// //----------------- Arithmetic
+// // var a = 1 * 2
+
+// // console.log(a)
+// //------------------Assigment (= : gán giá trị)
+// // var fullName = 'Le Manh Cuong'
+
+// //-------------------Comparison
+// // var a = 1
+// // var b = 2
+
+// // if(a > b) {
+// //     //code
+// // }
+
+// //Logical--------- && cả 2 về đều đúng
+// // var a = 1
+// // var b = 2
+
+// // if (a > 0 && b >0) {
+// //    console.log('right')
+// // } else {
+// //    alert('wrong')
+// // }
+
+// // -- giảm 1 chỉ số 
+// // ++ tăng 1 chỉ số
+// // prefix : tiền tố ( trước )
+// // 
+// // postfix : hậu số ( sau )
+// // copy trước
+
+// // boolean
+// /** 6 giá trị đưa về false
+//  * 0
+//  * false
+//  * ''-""
+//  * undefined : không gán giá trị 
+//  * NaN
+//  * null
+//  */
+var a = 1
+var b = 2
+var result = a < b && a < 0 
+console.log (result)
